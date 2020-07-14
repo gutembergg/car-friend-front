ca67bdc3cfbd0de2f0c05826a4bbaaf881d666fb
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-15 01:40:47 +0200

    ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://car-friend.herokuapp.com/",
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: false,
     headers: {
         "Access-Control-Allow-Origin": "*",
