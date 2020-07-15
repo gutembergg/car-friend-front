eb6100dc1bb0956f82459d369435cdf87a0ac4d5
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-07-16 01:45:10 +0200

    test en-tete

--- src/components/addPassenger.js
+++ src/components/addPassenger.js
@@ -1,59 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import api from '../services/api';
-import ButtonAddPassenger from './buttonAddPassenger';
-import { MDBInput, MDBIcon, MDBView, MDBMask } from 'mdbreact';
+import api from "../services/api";
+import ButtonAddPassenger from "./buttonAddPassenger";
+import { MDBInput, MDBIcon, MDBView, MDBMask } from "mdbreact";
 
+const AddPassenger = ({
+    carId,
+    carSeats,
+    getPassengerInCar,
+    getCars,
+    eventId,
+    userPassEvent,
+    userPassengerInEvent,
+    userEvents,
+}) => {
+    const [name, setName] = useState("");
+    const [showInputAddPassenger, setShowInputAddPassenger] = useState(false);
+    const user = JSON.parse(localStorage.getItem("@caroster:user"));
+    const [isButtonDisabled, setIsButtonDisabled] = useState("");
 
-const AddPassenger = ({ carId, carSeats, getPassengerInCar, getCars, eventId, userPassEvent, userPassengerInEvent, userEvents }) => {
-    const [ name, setName ] = useState('');
-    const [ showInputAddPassenger, setShowInputAddPassenger  ] = useState(false);
-    const user = JSON.parse(localStorage.getItem('@caroster:user'));
-    const [ isButtonDisabled, setIsButtonDisabled ] = useState("");
-
+    console.log(carId);
 
     async function handlePassenger(e) {
         e.preventDefault();
         try {
             setIsButtonDisabled("disabled");
-            
-            if(name.length === 0) {
-                return null
+
+            if (name.length === 0) {
+                return null;
             }
 
-            if(user) {
-                const userPassengerInEvent = userPassEvent.filter(passUser => passUser._id === eventId );
+            if (user) {
+                const userPassengerInEvent = userPassEvent.filter(
+                    passUser => passUser._id === eventId
+                );
 
                 const userEventId = userEvents.filter(user => user._id === eventId);
 
-                if(userPassengerInEvent.length !== 0 || userEventId.length !== 0) {
-                    alert("Vous participez déjà de cet évenement")
-                    return null
+                if (userPassengerInEvent.length !== 0 || userEventId.length !== 0) {
+                    alert("Vous participez déjà de cet évenement");
+                    return null;
                 }
             }
-            
 
             await api.post(`/event/car/${carId}/passenger`, {
-                name
+                name,
             });
-                getCars();
-                userPassengerInEvent();
-                setShowInputAddPassenger(false);
-                getPassengerInCar();
-                setName('');
+            getCars();
+            userPassengerInEvent();
+            setShowInputAddPassenger(false);
+            getPassengerInCar();
+            setName("");
 
-            const token = await localStorage.getItem('@caroster:token');
-            if(token) {  
-                await api.post(`/user/${eventId}/passengerInEvent`, {}, {
-                    headers: {
-                        Authorization: `Bearer ${token}`
+            const token = await localStorage.getItem("@caroster:token");
+            if (token) {
+                await api.post(
+                    `/user/${eventId}/passengerInEvent`,
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
                     }
-                });
+                );
             }
             setIsButtonDisabled("");
         } catch (error) {
             console.log(error);
-        } 
+        }
     }
 
     function showAddPassenger() {
@@ -61,49 +75,56 @@ const AddPassenger = ({ carId, carSeats, getPassengerInCar, getCars, eventId, us
     }
 
     function handleName(e) {
-     setName(e.target.value);    
-     getPassengerInCar();
+        setName(e.target.value);
+        getPassengerInCar();
     }
 
-
-    return(
-        <div>      
-            {showInputAddPassenger &&    
-                <form data-toggle="validator">        
+    return (
+        <div>
+            {showInputAddPassenger && (
+                <form data-toggle="validator">
                     <div className="row">
-                        <div className="ml-4 mr-2">  
-                            <MDBInput 
+                        <div className="ml-4 mr-2">
+                            <MDBInput
                                 type="text"
                                 className="ml-1 mt-n3 mb-n3"
                                 label="name"
                                 name="name"
-                                value={name.length > 22 ? '' : name}
+                                value={name.length > 22 ? "" : name}
                                 onChange={handleName}
                                 required
-                            />                   
+                            />
                         </div>
-                        <div >
-                            <MDBView hover onClick={handlePassenger} className={`${isButtonDisabled}`}>
-                                <MDBIcon icon="check" style={{color: "grey"}} className="mt-4 ml-4" />                
-                                 <MDBMask className="flex-center" overlay="white-strong"> 
-                                 </MDBMask>
+                        <div>
+                            <MDBView
+                                hover
+                                onClick={handlePassenger}
+                                className={`${isButtonDisabled}`}
+                            >
+                                <MDBIcon
+                                    icon="check"
+                                    style={{ color: "grey" }}
+                                    className="mt-4 ml-4"
+                                />
+                                <MDBMask className="flex-center" overlay="white-strong"></MDBMask>
                             </MDBView>
-                           
                         </div>
                         <div>
                             <MDBView hover onClick={showAddPassenger}>
-                            <MDBIcon icon="times"  style={{color: "grey"}} className="mt-4 ml-3" />
-                                    <MDBMask className="flex-center" overlay="white-strong"> 
-                                    </MDBMask>
+                                <MDBIcon
+                                    icon="times"
+                                    style={{ color: "grey" }}
+                                    className="mt-4 ml-3"
+                                />
+                                <MDBMask className="flex-center" overlay="white-strong"></MDBMask>
                             </MDBView>
                         </div>
-                    </div>   
-                </form>           
-            }
-            <ButtonAddPassenger carSeats={carSeats} showAddPassenger={showAddPassenger}/>
+                    </div>
+                </form>
+            )}
+            <ButtonAddPassenger carSeats={carSeats} showAddPassenger={showAddPassenger} />
         </div>
     );
-
-}
+};
 
 export default AddPassenger;
--- src/components/eventCars.js
+++ src/components/eventCars.js
@@ -6,124 +6,123 @@ import AddPassenger from "../components/addPassenger";
 import PassengerInCar from "../components/passengerInCar";
 import api from "../services/api";
 
-const EventCars = (props) => {
-  const [passengers, setPassengers] = useState([]);
+const EventCars = props => {
+    const [passengers, setPassengers] = useState([]);
 
-  async function getPassengerInCar() {
-    try {
-      const response = await api.get(`/event/car/${props.carId}/passenger`);
+    async function getPassengerInCar() {
+        try {
+            const response = await api.get(`/event/car/${props.carId}/passenger`);
 
-      setPassengers(response.data);
-    } catch (error) {
-      console.log(error);
+            setPassengers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  useEffect(() => {
-    getPassengerInCar();
-    props.getCars();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        props.getCars();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  useEffect(() => {
-    getPassengerInCar();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.carPlaces]);
+    useEffect(() => {
+        getPassengerInCar();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.carPlaces]);
 
-  const numberPassengers = passengers.length;
+    const numberPassengers = passengers.length;
 
-  return (
-    <div>
-      <div className="row mx-auto">
-        <div className="m-2">
-          <div className="card" style={{ width: "18rem" }}>
-            <div className="card-header">
-              <div className="row d-flex justify-content-between">
-                <div>
-                  <MDBIcon className="mr-2 ml-2" icon="car" />
-                  {props.carName}
+    return (
+        <div>
+            <div className="row mx-auto">
+                <div className="m-2">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <div className="card-header">
+                            <div className="row d-flex justify-content-between">
+                                <div>
+                                    <MDBIcon className="mr-2 ml-2" icon="car" />
+                                    {props.carName}
+                                </div>
+                                <div>
+                                    <Link
+                                        className="blue-grey-text"
+                                        to={{
+                                            pathname: `/car/edit`,
+                                            state: {
+                                                carId: props.carId,
+                                                carName: props.carName,
+                                                seats: props.seats,
+                                                contact: props.contact,
+                                                email: props.email,
+                                                address: props.address,
+                                                message: props.message,
+                                                date: props.date,
+                                                time: props.time,
+                                                eventId: props.eventId,
+                                            },
+                                        }}
+                                    >
+                                        <MDBIcon className="mr-1" icon="pencil-alt" />
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card-body border">
+                            <div className="card-text">
+                                <MDBIcon icon="chair" className="mr-3" />
+                                {props.seats}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon icon="phone-alt" className="mr-3" />
+                                {props.contact}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon far icon="envelope" className="mr-3" />
+                                {props.email.substr(0, 30)}
+                                <br />
+                                {props.email.substr(30)}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon far icon="comment-alt" className="mr-3" />
+                                {props.message}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon icon="map-marker-alt" className="mr-3" />
+                                {props.address}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon far icon="calendar-alt" className="mr-3" />
+                                {props.date}
+                            </div>
+                            <div className="card-text">
+                                <MDBIcon far icon="clock" className="mr-3" />
+                                {props.time}
+                            </div>
+                        </div>
+                        <PassengerInCar
+                            eventId={props.eventId}
+                            getCars={props.getCars}
+                            carFreePlace={props.carFreePlace}
+                            passengersCar={passengers}
+                            getPassengerInCar={getPassengerInCar}
+                        />
+                        <AddPassenger
+                            eventId={props.eventId}
+                            getCars={props.getCars}
+                            carFreePlace={props.carFreePlace}
+                            carId={props.carId}
+                            carSeats={
+                                numberPassengers ? props.seats - numberPassengers : props.seats
+                            }
+                            getPassengerInCar={getPassengerInCar}
+                            userPassEvent={props.userPassEvent}
+                            userPassengerInEvent={props.userPassengerInEvent}
+                            userEvents={props.userEvents}
+                        />
+                    </div>
                 </div>
-                <div>
-                  <Link
-                    className="blue-grey-text"
-                    to={{
-                      pathname: `/car/edit`,
-                      state: {
-                        carId: props.carId,
-                        carName: props.carName,
-                        seats: props.seats,
-                        contact: props.contact,
-                        email: props.email,
-                        address: props.address,
-                        message: props.message,
-                        date: props.date,
-                        time: props.time,
-                        eventId: props.eventId,
-                      },
-                    }}
-                  >
-                    <MDBIcon className="mr-1" icon="pencil-alt" />
-                  </Link>
-                </div>
-              </div>
             </div>
-            <div className="card-body border">
-              <div className="card-text">
-                <MDBIcon icon="chair" className="mr-3" />
-                {props.seats}
-              </div>
-              <div className="card-text">
-                <MDBIcon icon="phone-alt" className="mr-3" />
-                {props.contact}
-              </div>
-              <div className="card-text">
-                <MDBIcon far icon="envelope" className="mr-3" />
-                {props.email.substr(0, 30)}
-                <br />
-                {props.email.substr(30)}
-              </div>
-              <div className="card-text">
-                <MDBIcon far icon="comment-alt" className="mr-3" />
-                {props.message}
-              </div>
-              <div className="card-text">
-                <MDBIcon icon="map-marker-alt" className="mr-3" />
-                {props.address}
-              </div>
-              <div className="card-text">
-                <MDBIcon far icon="calendar-alt" className="mr-3" />
-                {props.date}
-              </div>
-              <div className="card-text">
-                <MDBIcon far icon="clock" className="mr-3" />
-                {props.time}
-              </div>
-            </div>
-            <PassengerInCar
-              eventId={props.eventId}
-              getCars={props.getCars}
-              carFreePlace={props.carFreePlace}
-              passengersCar={passengers}
-              getPassengerInCar={getPassengerInCar}
-            />
-            <AddPassenger
-              eventId={props.eventId}
-              getCars={props.getCars}
-              carFreePlace={props.carFreePlace}
-              carId={props.carId}
-              carSeats={
-                numberPassengers ? props.seats - numberPassengers : props.seats
-              }
-              getPassengerInCar={getPassengerInCar}
-              userPassEvent={props.userPassEvent}
-              userPassengerInEvent={props.userPassengerInEvent}
-              userEvents={props.userEvents}
-            />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default EventCars;
